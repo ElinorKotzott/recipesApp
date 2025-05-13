23fa241bf0c3d9863b28c02ce9fb25bc24738913
Eli_Nor <elinor.kotzott@gmx.de>
2025-05-13 12:29:58 +0200

    created comment entity and removed secret from .properties. untracked .properties file

--- src/main/java/com/elinor/recipes/model/Comment.java
+++ src/main/java/com/elinor/recipes/model/Comment.java
@@ -3,33 +3,29 @@ package com.elinor.recipes.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.Date;
 
 @Data
 @Entity
-@Table(name = "recipes")
-public class Recipe {
+@Table(name = "comments")
+public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
-    @Column(name = "title", length = 100, nullable = false)
-    private String title;
-    @Column(name = "description", length = 300)
-    private String description;
-    @Column(name = "prep_time")
-    private Integer prepTime;
-    @Column(name = "cooking_time")
-    private Integer cookingTime;
-    @Lob
-    @Column(name = "image")
-    private String image;
-    @CreationTimestamp
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipe_id", nullable = false)
+    private Recipe recipe;
+    @Column(name = "text")
+    private String text;
+
     @Column(name = "created_at", nullable = false, updatable = false)
     private Date createdAt;
 }
+
 
