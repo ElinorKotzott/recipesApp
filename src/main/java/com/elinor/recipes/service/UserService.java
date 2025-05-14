bad9aff50bcf18b59b642b3b9504826414f8d583
Eli_Nor <elinor.kotzott@gmx.de>
2025-05-14 15:08:07 +0200

    created service for user

---
+++ src/main/java/com/elinor/recipes/service/UserService.java
@@ -0,0 +1,20 @@
+package com.elinor.recipes.service;
+
+import com.elinor.recipes.model.User;
+import com.elinor.recipes.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
