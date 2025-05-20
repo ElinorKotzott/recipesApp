e1e18340554509cf8dd006e52cce7b157fa4b44f
Eli_Nor <elinor.kotzott@gmx.de>
2025-05-20 18:31:24 +0200

    replaced swagger config by open api config, added authorization with jwt on swagger ui

---
+++ src/main/java/com/elinor/recipes/config/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package com.elinor.recipes.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * to be able to test routes where login is required on swagger.
+ * will allow authentication via jwt.
+ * also, added custom title and description for swagger.
+ */
+
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("My Recipe App✨")
+                        .version("1.0")
+                        .description("API documentation for my full-stack project"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                );
+    }
+}
+
