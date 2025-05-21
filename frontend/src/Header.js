2c91b1f21cd6fcc6164ae78722c884888a81669b
Eli_Nor <elinor.kotzott@gmx.de>
2025-05-21 19:39:22 +0200

    trying out react. setting up connection between backend and react

---
+++ frontend/src/Header.js
@@ -0,0 +1,20 @@
+function Header() {
+
+    return(
+        <div class="header-container-class">
+            <header>
+                <h1>My website</h1>
+                <nav class="my-nav">
+                    <a href="#">Home</a>
+                    <a href="#">Create</a>
+                    <a href="#">Services</a>
+                    <a href="#">Contact</a>
+                </nav>
+            <hr></hr>
+            </header>
+        </div>
+    );
+}
+
+
+export default Header
