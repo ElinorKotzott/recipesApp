9ec957de276baaccd31c55f288b45d0c2f364308
Eli_Nor <elinor.kotzott@gmx.de>
2025-05-25 17:37:55 +0200

    setting up multi-page website using react router

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header.js'
+import Register from './components/Register'
+import Login from './components/Login'
+
+
+function App() {
+    return (
+        <Router>
+        <>
+            <Header/>
+            <Routes>
+                <Route path="/register" element={<Register/>} />
+                <Route path="/login" element={<Login/>} />
+            </Routes>
+        </>
+        </Router>
+    );
+}
+
+export default App;
+
+
