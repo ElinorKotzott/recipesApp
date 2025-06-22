d52adb33edab1f7e15480dc4dd1fd78a3c38f3e8
Eli_Nor <elinor.kotzott@gmx.de>
2025-06-22 21:05:46 +0200

    working on favoritesPage

--- frontend/src/pages/FavoritesPage.js
+++ frontend/src/pages/FavoritesPage.js
@@ -1,31 +1,46 @@
+import { useState, useEffect } from 'react';
+import { request } from '../axiosHelper';
+
 function FavoritesPage() {
+    const [favorites, setFavorites] = useState([]);
+    const token = sessionStorage.getItem('token');
 
     useEffect(() => {
+
             const fetchFavorites = async () => {
                 if (!token) return;
                 try {
-                    const response = await request('get', '/favorites', null, true);
                     const response = await request('get', '/favorites', null, true);
                     const favorites = response.data;
                     setFavorites(favorites);
 
                 } catch (error) {
-                    console.error('Error fetching profile:', error);
+                    console.error('Error fetching favs:', error);
                 }
             };
 
-            fetchProfile();
-        }, [token, location.state]);
+            fetchFavorites();
+        }, [token]);
 
 
-
-
-    return(
-        <div id="favs-container-id">
-                <p>Favs will be shown here</p>
-                <p>lalala</p>
-        </div>
-    );
+    return (
+            <div id="favs-container">
+                <h2>Your Favorite Recipes</h2>
+                {favorites.length === 0 ? (
+                    <p>No favorites found.</p>
+                ) : (
+                    <ul>
+                        {favorites.map(recipe => (
+                            <li key={recipe.id}>
+                                <h3>{recipe.title}</h3>
+                                <p>{recipe.description}</p>
+                                <p>Prep: {recipe.prepTime} min | Cook: {recipe.cookingTime} min</p>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        );
 }
 
 export default FavoritesPage;
