696e48d3f0a460f33a0f64dae25dc6fc44c3bef6
Eli_Nor <elinor.kotzott@gmx.de>
2025-06-23 21:00:09 +0200

    working on getting and depicting recipes on frontend

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,14 +1,31 @@
+import { useEffect, useState } from 'react';
 import LogoutButton from '../components/LogoutButton';
 import Recipe from '../components/Recipe';
-
+import { request } from '../axiosHelper';
 
 function HomePage () {
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            try {
+                const response = await request('get', '/recipes', null, true);
+                setRecipes(response.data);
+            } catch (error) {
+                console.error('Failed to fetch recipes:', error);
+            }
+        };
+
+        fetchRecipes();
+    }, []);
+
     return (
         <>
-            <LogoutButton/>
-            <Recipe/>
+            <LogoutButton />
+            <Recipe recipes={recipes} />
         </>
     );
 }
 
 export default HomePage;
+
