cac9d811f039b28996e8038a7362fed194f227eb
Eli_Nor <elinor.kotzott@gmx.de>
2025-06-23 20:02:48 +0200

    working on getting and depicting recipes on frontend

--- frontend/src/components/Recipe.js
+++ frontend/src/components/Recipe.js
@@ -1,9 +1,27 @@
-function Recipe () {
-    return(
-        <div className='recipe-card'>
-            <p>Hello i'm a recipe card</p>
+function Recipe({ recipes }) {
+    return (
+        <div className="recipe-card-container">
+            {recipes.length === 0 ? (
+                <p>Nothing to see here yet.</p>
+            ) : (
+                <ul>
+                    {recipes.map(recipe => (
+                        <li key={recipe.id}>
+                            <h3>{recipe.title}</h3>
+                            <p>{recipe.description}</p>
+                            <div className="tags">
+                                {recipe.tags.map((tag, index) => (
+                                    <span key={index} className="tag">
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
-    )
+    );
 }
 
 export default Recipe;
--- src/main/java/com/elinor/recipes/repository/RecipeRepository.java
+++ src/main/java/com/elinor/recipes/repository/RecipeRepository.java
@@ -1,7 +1,6 @@
 package com.elinor.recipes.repository;
 
 import com.elinor.recipes.model.Recipe;
-import lombok.NonNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
-    Page<Recipe> findAll(@NonNull Pageable pageable);
+    Page<Recipe> findAll(Pageable pageable);
 
     Page<Recipe> findByUserUsername(String username, Pageable pageable);
 }
