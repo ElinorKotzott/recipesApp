82facc8448b96cbad8b19748d9d3765d3ab873b0
Eli_Nor <elinor.kotzott@gmx.de>
2025-06-24 12:51:38 +0200

    created page info dto for pagination

---
+++ src/main/java/com/elinor/recipes/dto/PageInfoDTO.java
@@ -0,0 +1,17 @@
+package com.elinor.recipes.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageInfoDTO {
+    private List<RecipeDTO> recipes;
+    private int currentPage;
+    private int totalPages;
+    private long totalElements;
+}
